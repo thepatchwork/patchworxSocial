115fca87715e3bd1c207f76154f81357aa9185cb
Paul Denton <paul@thepatchworx.co.uk>
2016-11-17 12:06:58 +0000

    completed setting up firebase integration

---
+++ PatchworxSocial/SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignInViewController.swift
+//  PatchworxSocial
+//
+//  Created by Paul Denton on 17/11/2016.
+//  Copyright © 2016 patchworx. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
