82ec3b783adfd51de00f7b8659e695d9041c74e5
Paul Denton <paul@thepatchworx.co.uk>
2016-11-22 09:43:27 +0000

    feed ui finished

--- PatchworxSocial/CircleView.swift
+++ PatchworxSocial/CircleView.swift
@@ -1,24 +1,26 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  PatchworxSocial
 //
-//  Created by Paul Denton on 17/11/2016.
+//  Created by Paul Denton on 22/11/2016.
 //  Copyright © 2016 patchworx. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
